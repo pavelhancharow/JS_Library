7d0f92684edcf63db10c7de0ecd04e2ebb82b5b8
Pavel <pl.hancharou@gmail.com>
2021-01-11 17:31:05 +0300

    feat: create attribute set/remove/get

---
+++ src/js/lib/modules/attribute.js
@@ -0,0 +1,22 @@
+import $ from '../core';
+
+$.prototype.setAttr = function (attrName, attrValue) {
+  for (let i = 0; i < this.length; i++) {
+    this[i].setAttribute(attrName, attrValue);
+  }
+  return this;
+};
+
+$.prototype.removeAttr = function (attrName) {
+  for (let i = 0; i < this.length; i++) {
+    this[i].removeAttribute(attrName);
+  }
+  return this;
+};
+
+$.prototype.getAttr = function (attrName) {
+  for (let i = 0; i < this.length; i++) {
+    this[i].getAttribute(attrName);
+  }
+  return this;
+};

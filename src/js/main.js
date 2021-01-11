87c5b52817649377db0e68e341654a4eaab40f23
Pavel <pl.hancharou@gmail.com>
2021-01-11 17:28:45 +0300

    fix: test modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-import "./lib/lib";
+import $ from "./lib/lib";
 
-$('.active').show();
+$('button').click(function () {
+  $(this).toggleClass('active');
+  $(this).setAttr('data-btn', 'push');
+  setTimeout(() => {
+    $(this).removeAttr('data-btn');
+  }, 1000);
+});

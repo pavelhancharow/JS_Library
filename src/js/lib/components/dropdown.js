1521d067976d436ae5c8e697512925f5a2f174e4
Pavel <pl.hancharou@gmail.com>
2021-01-12 18:01:22 +0300

    fix: fix dropdown method

--- src/js/lib/components/dropdown.js
+++ src/js/lib/components/dropdown.js
@@ -2,10 +2,12 @@ import $ from '../core';
 
 $.prototype.dropdown = function () {
   for (let i = 0; i < this.length; i++) {
-    const id = $(this[i]).getAttr('id').id;
+    const id = $(this[i]).getAttr('id');
 
     $(this[i]).click(() => {
       $(`[data-toggle-id=${id}]`).fadeToggle(300);
     });
   }
 };
+
+$('.dropdown-toggle').dropdown();

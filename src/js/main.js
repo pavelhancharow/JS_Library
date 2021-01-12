c61921bbffd34b020f93cfa73451b847aa32bc4d
Pavel <pl.hancharou@gmail.com>
2021-01-12 15:58:55 +0300

    fix: test dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,16 @@ $('[data-count="second"]').click(() => {
 $('button').eq(2).click(() => {
   $('.w-500').fadeToggle(800);
 });
+
+$('.wrap').html(`
+  <div class="dropdown">
+    <div class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</div>
+    <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+      <a href="#" class="dropdown-item">Action</a>
+      <a href="#" class="dropdown-item">Action #2</a>
+      <a href="#" class="dropdown-item">Action #3</a>
+    </div>
+  </div>
+`);
+
+$('.dropdown-toggle').dropdown();
